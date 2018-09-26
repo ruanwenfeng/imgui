5b3f09597ec76fefa314bd24a9dbf7acb575ba12
ruanwenfeng <ruanwenfeng@hotmail.com>
2018-09-26 23:41:50 +0800

    最大化一个window

--- demo/win32_hello_world/main.cpp
+++ demo/win32_hello_world/main.cpp
@@ -174,33 +174,54 @@ int main(int, char**)
 		ImGui_ImplWin32_NewFrame();
 		ImGui::NewFrame();
 		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
+		//if (show_demo_window)
+		//	ImGui::ShowDemoWindow(&show_demo_window);
 
-		if (ImGui::BeginMainMenuBar()) {
-			if (ImGui::BeginMenu("File")) {
-				ImGui::MenuItem("(dummy menu)", NULL, false, false);
-				ImGui::EndMenu();
-			}
-			if (ImGui::BeginMenu("Edit")) {
-				if (ImGui::MenuItem("Undo", "CTRL+Z")) {
-					MessageBox(hwnd, TEXT("Undo"), TEXT("CTRL+Z"), 0);
-				}
-				if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
-				ImGui::Separator();
-				if (ImGui::MenuItem("Cut", "CTRL+X")) {}
-				if (ImGui::MenuItem("Copy", "CTRL+C")) {}
-				if (ImGui::MenuItem("Paste", "CTRL+V")) {}
-				ImGui::EndMenu();
-			}
-			ImGui::EndMainMenuBar();
-		}
+		
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		float cxClient = rect.right - rect.left;  // 获得客户区宽度
+		float cyClient = rect.bottom - rect.top;  // 获得客户区高度
+		ImGui::SetWindowSize("Hello, world!", { cxClient,cyClient });
+		ImGui::SetWindowPos("Hello, world!", { 0,0 });
+		
 		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 		{
 			static float f = 0.0f;
 			static int counter = 0;
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_MenuBar;
+			//if (no_titlebar)        window_flags |= ImGuiWindowFlags_NoTitleBar;
+			//if (no_scrollbar)       window_flags |= ImGuiWindowFlags_NoScrollbar;
+			//if (!no_menu)           window_flags |= ImGuiWindowFlags_MenuBar;
+			//if (no_move)            window_flags |= ImGuiWindowFlags_NoMove;
+			//if (no_resize)          window_flags |= ImGuiWindowFlags_NoResize;
+			//if (no_collapse)        window_flags |= ImGuiWindowFlags_NoCollapse;
+			//if (no_nav)             window_flags |= ImGuiWindowFlags_NoNav;
+			//if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+			//if (no_close)           p_open = NULL; // Don't pass our bool* to Begin
+
+			ImGui::Begin("Hello, world!",nullptr, window_flags);                          // Create a window called "Hello, world!" and append into it.
+			
+			if (ImGui::BeginMenuBar()) {
+				if (ImGui::BeginMenu("File")) {
+					ImGui::MenuItem("fish_hat.c");
+					ImGui::MenuItem("fish_hat.inl");
+					ImGui::MenuItem("fish_hat.h");
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Edit")) {
+					if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+					if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+					ImGui::Separator();
+					if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+					if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+					if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+					ImGui::EndMenu();
+				}
+				ImGui::EndMenuBar();
+			}
+
 			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
 			ImGui::Checkbox("Another Window", &show_another_window);
@@ -217,14 +238,14 @@ int main(int, char**)
 			ImGui::End();
 		}
 
-		// 3. Show another simple window.
-		if (show_another_window) {
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
+		//// 3. Show another simple window.
+		//if (show_another_window) {
+		//	ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		//	ImGui::Text("Hello from another window!");
+		//	if (ImGui::Button("Close Me"))
+		//		show_another_window = false;
+		//	ImGui::End();
+		//}
 
 		// Rendering
 		ImGui::Render();
